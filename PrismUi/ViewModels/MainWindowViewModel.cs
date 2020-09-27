1351aa6ae11c8ff18d96907304a10f5d3c4f06bf
Christoph Hilmer <hilmerchristoph@googlemail.com>
2020-09-27 03:29:21 +0200

    Add project files.

---
+++ PrismUi/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace PrismUi.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "CEBP";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
