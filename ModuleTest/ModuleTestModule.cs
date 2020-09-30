64050e182e1f4477f7d2c5917875154096c5ddaa
Christoph Hilmer <hilmerchristoph@googlemail.com>
2020-09-30 21:54:43 +0200

    Add View Injection

--- ModuleTest/ModuleTestModule.cs
+++ ModuleTest/ModuleTestModule.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
 using ModuleTest.Views;
 using Prism.Ioc;
 using Prism.Modularity;
@@ -25,6 +27,24 @@ namespace ModuleTest
         public void OnInitialized(IContainerProvider containerProvider)
         {
             _regionManager.RegisterViewWithRegion("ContentRegion", typeof(TestView));
+            
+            IRegion region = _regionManager.Regions["SecondRegion"];
+            var secondView = containerProvider.Resolve<SecondView>();
+            region.Add(secondView);
+
+            var thirdView = containerProvider.Resolve<TestView>();
+            thirdView.Content = new TextBlock()
+            {
+                Text = "Hello from ThirdView",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            region.Add(thirdView);
+            region.Activate(thirdView);
+
+            region.Activate(secondView);
+            region.Deactivate(secondView);
         }
     }
 }
