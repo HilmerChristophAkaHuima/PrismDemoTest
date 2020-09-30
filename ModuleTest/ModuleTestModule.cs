7851789719a8114269ecff25c16d9c035903808e
Christoph Hilmer <hilmerchristoph@googlemail.com>
2020-09-30 21:32:18 +0200

    Add View Discovery

--- ModuleTest/ModuleTestModule.cs
+++ ModuleTest/ModuleTestModule.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ModuleTest.Views;
 using Prism.Ioc;
 using Prism.Modularity;
+using Prism.Regions;
 
 namespace ModuleTest
 {
     public class ModuleTestModule : IModule
     {
+        private readonly IRegionManager _regionManager;
+
+        public ModuleTestModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            throw new NotImplementedException();
+            
         }
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
-            throw new NotImplementedException();
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(TestView));
         }
     }
 }
